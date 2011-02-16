41ba1228b1bdc7693a08e9ba20e0dcf4acec0db7
Viktor Klang <viktor.klang@gmail.com>
2011-02-16 15:07:30 +0100

    Updating to Akka 1.0

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akka_repo  = "Akka Maven Repository" at "http://akka.io/repository"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0-RC3"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0"
 }
